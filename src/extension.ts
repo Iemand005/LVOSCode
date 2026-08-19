b6af084acf7c7c366de8b780887f84b70fa00e34
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-08-19 22:42:16 +0200

    yay works

--- src/extension.ts
+++ src/extension.ts
@@ -45,7 +45,7 @@ function getWebviewContent() {
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Custom Window</title>
+		<title>LVOS</title>
 		<style>
 			html, body {
 				display: flex;
